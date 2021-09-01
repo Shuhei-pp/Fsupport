3346cf4ac6ef24c8d5cff7dba409906a04cf9074
Shuhei-pp <lotakotayota@gmail.com>
2021-09-01 09:29:51 +0900

    getweatherで天気を取得可能に

--- README.md
+++ README.md
@@ -15,4 +15,6 @@ $ docker-compose run app php artisan migrate　# マイグレーション
 
 # 参考にしたサイト
 
-https://qiita.com/ucan-lab/items/56c9dc3cf2e6762672f4
+https://qiita.com/ucan-lab/items/56c9dc3cf2e6762672f4  
+  
+https://hsmt-web.com/blog/openweathermap-api/
--- backend/app/Services/GetWeather.php
+++ backend/app/Services/GetWeather.php
@@ -5,7 +5,12 @@ class GetWeather
 {
   public function index()
   {
-    echo 'テスト';
+    $city_zip = "950-2102,jp";
+    $apiid = "d7686001f05a0d1327e22d449b4f6282";
+    $url = "http://api.openweathermap.org/data/2.5/weather?zip=".$city_zip."&units=metric&APPID=".$apiid;
+
+    $response = file_get_contents($url);
+    echo $response;
   }
 }
 
