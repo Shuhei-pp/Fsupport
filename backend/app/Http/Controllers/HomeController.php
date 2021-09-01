44b56569355acf63d92a8f8d9986a465aaa526c6
Shuhei-pp <lotakotayota@gmail.com>
2021-09-01 20:41:39 +0900

    controllerで変数私

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -10,7 +10,8 @@ class HomeController extends Controller
     public function index()
     {
         $weather = new GetWeather();
-        $weather_info = $weather->index();
+        $city_zip = "950-2102,jp";
+        $weather_info = $weather->index($city_zip);
         $current_weather = $weather_info->weather[0]->main;
         return view('home',[
             "current_weather" => $current_weather
