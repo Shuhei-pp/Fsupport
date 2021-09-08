241f5dbd20c4ce45f2b6cf6559281541fd1fb77a
Shuhei-pp <lotakotayota@gmail.com>
2021-09-08 20:48:15 +0900

    ログインへのルーティングを修正

--- backend/resources/views/Index.blade.php
+++ backend/resources/views/Index.blade.php
@@ -1,7 +1,6 @@
 @include('common.head')
 <body>
 @include('common.header')
-<a href="/area/950-2102,jp">Shinkawa</a>
 <?php
   foreach($area_info as $info){
     echo '<a href="/area/'.$info->area_zip.'">'.$info->area_name.'</a></br>';
