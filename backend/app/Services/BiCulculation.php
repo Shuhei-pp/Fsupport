1e763c8b28f9e038af48fccb3cfc9fd3254f1e51
Shuhei-pp <lotakotayota@gmail.com>
2021-09-11 18:57:46 +0900

    3時間ごとの海面の高さ(tide)を表示するクラスを作成

--- backend/app/Services/BiCulculation.php
+++ backend/app/Services/BiCulculation.php
@@ -7,17 +7,26 @@ class BiCulculation
   {
     date_default_timezone_set('Asia/Tokyo');
     $today = date("Y-m-d");
+    $tomorrow = date("Y-m-d", strtotime('+1 day'));
+
     /*APIの海面の高さが20分ごとで配列にされているので、
     3時間ごとで得られるように、$factorに9をセットしておく*/
     $factor = 9;
 
-    $start_time = date("G:i:s");
-    var_dump($start_time);
+    $start_tide_time = round(date("G")/3)+1;
+
+    $counts_assign = 0;
 
     //apiで得た潮の干満(cm)を$every_3hour_tideにセット
-    for($i=1;$i<=8;$i++){
+    for($i=$start_tide_time;$i<=$start_tide_time+7;$i++){
       $every_3hour_tide[] = $tide_info->tide->chart->$today->tide[$i*$factor]->cm;
+      $counts_assign++;
+      if($i>7)
+        break;
     }
+
+    for($i=1;$i<=8-$counts_assign;$i++)
+      $every_3hour_tide[] = $tide_info->tide->chart->$tomorrow->tide[$i*$factor]->cm;
 
     return $every_3hour_tide;
   }
