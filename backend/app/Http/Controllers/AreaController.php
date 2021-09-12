a190a8564259506ca8d5193234e5551d8f6b37a0
Shuhei-pp <lotakotayota@gmail.com>
2021-09-12 16:43:14 +0900

    エリアごとで海面高さを表示

--- backend/app/Http/Controllers/AreaController.php
+++ backend/app/Http/Controllers/AreaController.php
@@ -1,19 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Area;
+
 use App\Services\GetWeather;
 use App\Services\GetTide;
 use App\Services\BiCulculation;
+
+
 use Illuminate\Http\Request;
 
 class AreaController extends Controller
 {
-    public function index($area_zip){
+    public function index($area_id){
+
+        $area = Area::find($area_id);
 
         $weather = new GetWeather();
-        $weather_info = $weather->getWeatherJson($area_zip); 
+        $weather_info = $weather->getWeatherJson($area->area_zip);
+
+
         $tide = new GetTide();
-        $tide_info = $tide->getTideJson();
+        $tide_info = $tide->getTideJson($area_id);
 
         $tide_heights = $tide->setTide($tide_info);
         return view('area',compact('weather_info','tide_heights'));
