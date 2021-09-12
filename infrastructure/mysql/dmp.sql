8af2027dce64bbcf82203998358e6373b03ca3fb
Shuhei-pp <lotakotayota@gmail.com>
2021-09-12 15:33:38 +0900

    村上市の住所がapi非対応だったのでdumpファイルを修正

--- infrastructure/mysql/dmp.sql
+++ infrastructure/mysql/dmp.sql
@@ -2,7 +2,7 @@ INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `create
 INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (15, 5, '巻', '953-0012,jp', NOW(), NOW());
 INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (15, 5, '寺泊', '940-2502,jp', NOW(), NOW());
 INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (15, 13, '新潟東港', '957-0101,jp', NOW(), NOW());
-INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (15, 11, '岩船', '958-0000,jp', NOW(), NOW());
+INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (15, 11, '岩船', '958-0058,jp', NOW(), NOW());
 
 INSERT INTO `bigareas` (`bigarea_id`,`bigarea_name`,`created_at`,`updated_at`) VALUES (15,'新潟県',NOW(),NOW());
 
