f30d6a11c3d739aeea7bf0dd8f0956573e0215e6
Shuhei-pp <lotakotayota@gmail.com>
2021-09-14 09:08:45 +0900

    sinkawaを消去

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @include('common.head')
 <body>
 @include('common.header')
-<a href="/area/950-2102,jp">Shinkawa</a>
 <?php
   foreach($area_info as $info){
     echo '<a href="/area/'.$info->id.'">'.$info->area_name.'</a></br>';
