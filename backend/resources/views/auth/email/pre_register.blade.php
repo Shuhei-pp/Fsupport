9ff2c962b835eb346028da3195c3ecbcf47c5ab7
Shuhei-pp <lotakotayota@gmail.com>
2021-09-15 17:52:40 +0900

    mailview修正

--- backend/resources/views/auth/email/pre_register.blade.php
+++ backend/resources/views/auth/email/pre_register.blade.php
@@ -1,4 +1,4 @@
 サイトへの仮登録が完了しました。<br>
 <br>
 以下のURLからクリックして、登録を完了させてください。<br>
-{{url('register/verify/'.$token)}}
+<a href=<?php echo'register/verify/'.$token?>><?php echo'register/verify/'.$token?><a>
