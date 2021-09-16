6ba96f8b1002e6e11da33d1e1bb93ed078f338e1
Shuhei-pp <lotakotayota@gmail.com>
2021-09-16 15:02:06 +0900

    forの書き方がゴミだったので修正

--- backend/resources/views/area.blade.php
+++ backend/resources/views/area.blade.php
@@ -4,38 +4,33 @@
   <table border="1px">
     <tr>
       <th>data</th>
-      <?php 
-      for($i=3;$i<11;$i++)
-        echo "<th>".$weather_info->list[$i]->dt_txt."</th>";
-      ?>
+      <?php for($i=3;$i<11;$i++) {?>
+        <th>{{ $weather_info->list[$i]->dt_txt }}</th>
+      <?php } ?>
     </tr>
     <tr>
       <td>weather</td>
-      <?php 
-      for($i=3;$i<11;$i++)
-        echo "<td>".$weather_info->list[$i]->weather[0]->main."</td>";
-      ?>
+      <?php for($i=3;$i<11;$i++) {?>
+        <td>{{ $weather_info->list[$i]->weather[0]->main }}</td>
+      <?php } ?>
     </tr>
     <tr>
       <td>temp</td>
-      <?php
-      for($i=0;$i<8;$i++)
-        echo "<td>".$weather_info->list[$i]->main->temp."</td>";
-      ?>
+      <?php for($i=0;$i<8;$i++) {?>
+        <td>{{ $weather_info->list[$i]->main->temp }}</td>
+      <?php }?>
     </tr>
     <tr>
       <td>tide</td>
-      <?php
-        foreach($tide_heights as $tide_height)
-          echo "<td>".$tide_height."</td>";
-      ?>
+      <?php foreach($tide_heights as $tide_height) {?>
+        <td>{{ $tide_height }}</td>
+      <?php }?>
     </tr>
     <tr>
       <td>fi</td>
-      <?php
-        foreach($fi as $fi)
-          echo "<td>".$fi."</td>";
-      ?>
+      <?php foreach($fi as $fi) {?>
+        <td>{{ $fi }}</td>
+      <?php } ?>
     </tr>
   </table>
 @endsection
