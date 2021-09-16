beead29fe32741f8f0f402298f3e312370bd9c3f
Shuhei-pp <lotakotayota@gmail.com>
2021-09-16 13:18:38 +0900

    user_statusの定数を変更

--- backend/config/const.php
+++ backend/config/const.php
@@ -1,7 +1,7 @@
 <?php
 
-// 0:仮登録 1:本登録 2:メール認証済 9:退会済
+// 0:退会済 1:仮登録 2:メール認証済 9:管理人
 
 return [
-  'USER_STATUS' => ['PRE_REGISTER' => '0', 'REGISTER' => '1', 'MAIL_AUTHED' => '2', 'DEACTIVE' => '9',],
+  'USER_STATUS' => ['DEACTIVE' => '0', 'PRE_REGISTER' => '1', 'MAIL_AUTHED' => '2','MANAGER' => '9'],
 ];
