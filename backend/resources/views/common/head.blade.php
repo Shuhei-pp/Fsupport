fae5d064803ac43bd572d074eb1556b585d34148
Shuhei-pp <lotakotayota@gmail.com>
2021-09-16 13:36:04 +0900

    homeとareaのview修正

--- backend/resources/views/common/head.blade.php
+++
@@ -1,5 +0,0 @@
-
-<meta charset="utf-8">
-<link rel="stylesheet" href={{ asset("css/common.css") }}>
-<link rel="stylesheet" href={{ asset("/css/app.css") }}>
-<script src={{ asset("/js/app.js") }}></script>
--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <?php
   foreach($area_info as $info){
     echo '<a href="/area/'.$info->id.'">'.$info->area_name.'</a></br>';
