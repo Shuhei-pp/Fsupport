e7fcddbec57db48a2f4ab21300502c862b4571ae
Shuhei-pp <lotakotayota@gmail.com>
2021-09-17 14:50:54 +0900

    テンプレートにjsを記述するように

--- backend/resources/views/area.blade.php
+++ backend/resources/views/area.blade.php
@@ -49,5 +49,35 @@
 
 @section('script')
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  <script src="{{ mix('js/chart.js') }}"></script>
+  <script>
+    window.onload = function(){
+      const labels = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      ];
+      const data = {
+      labels: labels,
+      datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+      }]
+      };
+      const config = {
+      type: 'line',
+      data: data,
+      options: {}
+      };
+      console.log (66);
+      var myChart = new Chart(
+          document.getElementById('myChart'),
+          config
+      );
+    }
+  </script>
 @endsection
