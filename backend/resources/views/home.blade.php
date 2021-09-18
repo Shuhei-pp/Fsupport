3960e51be2cc719aef3f7f6d3ea7ab452e6ec513
Shuhei-pp <lotakotayota@gmail.com>
2021-09-18 15:53:55 +0900

    hemebladeの書き方修正

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
 
-<?php
-  foreach($area_info as $info){
-    echo '<a href="/area/'.$info->id.'">'.$info->area_name.'</a></br>';
-  }
-?>
+  <?php foreach($area_info as $info) { ?>
+    <a href="/area/{{ $info->id }}">{{ $info->area_name }}</a></br>
+  <?php } ?>
 @endsection
