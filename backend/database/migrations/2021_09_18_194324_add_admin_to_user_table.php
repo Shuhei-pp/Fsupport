5274b2a746b1754f82e261c27f1da41c065ea277
Shuhei-pp <lotakotayota@gmail.com>
2021-09-18 20:00:09 +0900

    adminをnullableに

--- backend/database/migrations/2021_09_18_194324_add_admin_to_user_table.php
+++ backend/database/migrations/2021_09_18_194324_add_admin_to_user_table.php
@@ -14,7 +14,7 @@ class AddAdminToUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('admin');
+            $table->integer('admin')->nullable();
         });
     }
 
