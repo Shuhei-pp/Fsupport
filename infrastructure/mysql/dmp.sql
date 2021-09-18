52dea5336e8a783a4cd3d820882a11fe3b6e0857
Shuhei-pp <lotakotayota@gmail.com>
2021-09-18 16:36:55 +0900

    dmpファイルに富山県エリアを追加

--- infrastructure/mysql/dmp.sql
+++ infrastructure/mysql/dmp.sql
@@ -3,8 +3,11 @@ INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `create
 INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (15, 5, '寺泊', '940-2502,jp', NOW(), NOW());
 INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (15, 13, '新潟東港', '957-0101,jp', NOW(), NOW());
 INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (15, 11, '岩船', '958-0058,jp', NOW(), NOW());
+INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (16, 2, '伏木', '933-0102,jp', NOW(), NOW());
+INSERT INTO `areas` (`bigarea_id`, `harbor_id`, `area_name`, `area_zip`, `created_at`, `updated_at`) VALUES (16, 3, '富山', '931-8378,jp', NOW(), NOW());
 
 INSERT INTO `bigareas` (`bigarea_id`,`bigarea_name`,`created_at`,`updated_at`) VALUES (15,'新潟県',NOW(),NOW());
+INSERT INTO `bigareas` (`bigarea_id`,`bigarea_name`,`created_at`,`updated_at`) VALUES (16,'富山県',NOW(),NOW());
 
 INSERT INTO `harbors` (`bigarea_id`,`harbor_id`,`harbor_name`,`created_at`,`updated_at`) VALUES (15,1,'能生',NOW(),NOW());
 INSERT INTO `harbors` (`bigarea_id`,`harbor_id`,`harbor_name`,`created_at`,`updated_at`) VALUES (15,2,'直江津',NOW(),NOW());
@@ -14,3 +17,5 @@ INSERT INTO `harbors` (`bigarea_id`,`harbor_id`,`harbor_name`,`created_at`,`upda
 INSERT INTO `harbors` (`bigarea_id`,`harbor_id`,`harbor_name`,`created_at`,`updated_at`) VALUES (15,6,'新潟西港',NOW(),NOW());
 INSERT INTO `harbors` (`bigarea_id`,`harbor_id`,`harbor_name`,`created_at`,`updated_at`) VALUES (15,11,'岩船',NOW(),NOW());
 INSERT INTO `harbors` (`bigarea_id`,`harbor_id`,`harbor_name`,`created_at`,`updated_at`) VALUES (15,13,'新潟東港',NOW(),NOW());
+INSERT INTO `harbors` (`bigarea_id`,`harbor_id`,`harbor_name`,`created_at`,`updated_at`) VALUES (16,2,'伏木',NOW(),NOW());
+INSERT INTO `harbors` (`bigarea_id`,`harbor_id`,`harbor_name`,`created_at`,`updated_at`) VALUES (16,3,'富山',NOW(),NOW());
