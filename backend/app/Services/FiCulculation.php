69a43b017c61c1f8977022b83c995f747c5709ef
Shuhei-pp <lotakotayota@gmail.com>
2021-09-22 14:58:35 +0900

    風の強さによって点数を返すfiWind関数作成

--- backend/app/Services/FiCulculation.php
+++ backend/app/Services/FiCulculation.php
@@ -3,6 +3,32 @@ namespace App\Services;
 
 class FiCulculation
 {
+
+  /**
+   * 風の強さによって点数を返すfiWind関数
+   */
+  public static function fiWind($wind)
+  {
+    if($wind >= 5)
+    {
+      return 10/$wind;
+    }
+    elseif($wind >= 1)
+    {
+      $wind_p = (10/$wind) + 1;
+      if($wind_p >= 10){
+        return 10;
+      }
+      return $wind_p;
+    }
+    elseif ($wind <= 0)
+    {
+      return 0;
+    } 
+    else
+    return 0;
+  }
+
   /**
    * 釣果指数fiをセットする
    * 
@@ -13,12 +39,15 @@ class FiCulculation
    */
   public static function setFi($weather,$tide)
   {
-    /**todo: この変数は結構使うので、他で定義したい*/
     $repeat_times = count($tide);
 
     //apiで得た潮の干満(cm)を$every_3hour_tideにセット
-    for($i=0;$i<$repeat_times;$i++)
+    for($i=0;$i<$repeat_times;$i++){
       $fi[] = round($tide[$i]*$weather->list[$i]->wind->speed);
+      var_dump($weather->list[$i]->wind->speed);
+      var_dump(round(self::fiWind($weather->list[$i]->wind->speed),2));
+      echo '|';
+    }
 
     return $fi;
   }
