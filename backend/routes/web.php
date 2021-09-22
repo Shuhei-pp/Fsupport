9feadffc3b7dcc113d87ebe1a954fcf5e5f308da
Shuhei-pp <lotakotayota@gmail.com>
2021-09-22 11:53:34 +0900

    web.phpが汚かったので整頓

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,21 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 // check: routeは->name()で名前をつけられる！　viewのURL修正も楽になるから修正しよう
-Route::post('/register/pre_check', 'App\Http\Controllers\Auth\RegisterController@preCheck')->name('register.pre_check');
-
-Route::get('/register/verify/{token}', 'App\Http\Controllers\Auth\RegisterController@showMyPage');
-
+//エリア系ルーティング
 Route::get('/', [App\Http\Controllers\AreaController::class,'index']);
 Route::get('/area/edit', [App\Http\Controllers\AreaController::class,'toEditAreaPage'])->name('area_edit');
 Route::post('/area/edit', [App\Http\Controllers\AreaController::class,'addArea']);
 Route::get('/area/{area_id}', [App\Http\Controllers\AreaController::class,'showArea'])->name('area.show');
 
+//ユーザー系ルーティング
 Route::get('/user/edit', [App\Http\Controllers\UsersController::class,'showEditPage'])->name('user.edit');
 Route::get('/user/mypage/{user_id}', [App\Http\Controllers\UsersController::class,'showMyPage'])->name('user.mypage');
 
+//釣果系ルーティング
 Route::post('/fresult/create', [App\Http\Controllers\FishingRecordController::class,'create'])->name('create.fresult');
 Route::get('/fresult/edit/{fresult_id}', [App\Http\Controllers\FishingRecordController::class, 'toEditPage'])->name('edit.fresult');
 
+Route::post('/register/pre_check', 'App\Http\Controllers\Auth\RegisterController@preCheck')->name('register.pre_check');
+Route::get('/register/verify/{token}', 'App\Http\Controllers\Auth\RegisterController@showMyPage');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
