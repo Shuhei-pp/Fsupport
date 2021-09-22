71da3355b41417f33bfd905f24e2eb6f71549362
Shuhei-pp <lotakotayota@gmail.com>
2021-09-22 19:20:59 +0900

    monthpoint実装

--- backend/app/Services/FiCulculation.php
+++ backend/app/Services/FiCulculation.php
@@ -7,11 +7,31 @@ class FiCulculation
   protected static $sunrise;
   protected static $sunset;
 
+  /**
+   * FiCulculation constructors
+   * 
+   * @param object $tide
+   */
   public function __construct($tide)
   {
     $today = date("Y-m-d");
     self::$sunrise = $tide->tide->chart->$today->sun->rise;
     self::$sunset = $tide->tide->chart->$today->sun->set;
+  }
+
+
+  private static function monthPoint($month)
+  {
+    if( 5<=$month && 11>$month)
+    {
+      return 3;
+    }
+    elseif( 3<=$month && 11>=$month )
+    {
+      return 2;
+    }
+    else
+      return 1;
   }
 
   /**
@@ -56,7 +76,13 @@ class FiCulculation
 
     //apiで得た潮の干満(cm)を$every_3hour_tideにセット
     for($i=0;$i<$repeat_times;$i++){
-      $fi[] = round($tide[$i]*$weather->list[$i]->wind->speed);
+      //月を取り出し、ポイントへと変換
+      $m = date('n', strtotime($weather->list[$i]->dt_txt));
+      $mp = self::monthPoint($m);
+
+      //windpointへ変換
+      $wp = self::fiWind($weather->list[$i]->wind->speed);
+      $fi[] = round($mp * $wp,2);
     }
     return $fi;
   }
