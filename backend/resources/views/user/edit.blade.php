07461a67948dda1dd049aa1cc856461d015b9b74
Shuhei-pp <lotakotayota@gmail.com>
2021-09-26 14:51:29 +0900

    ユーザー編集ページのbladeを表示

--- backend/resources/views/user/edit.blade.php
+++ backend/resources/views/user/edit.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-  
+
+<div class="container">
+  <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">ユーザー編集</div>
+
+          <div class="table-responsive">
+            <div class="card-body">
+              <table class=table>
+                <tbody>
+                  <thead>
+                    <th>
+                      i
+                    </th>
+                    <th>
+                      j
+                    <th>
+                  </thead>
+                  <tr>
+                    <td>1</td>
+                    <td>2</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
+
 @endsection
