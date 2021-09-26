5b862537e92fd86daa63dff992f307b2ef568de6
Shuhei-pp <lotakotayota@gmail.com>
2021-09-26 15:47:33 +0900

    userテーブルのデフォルト値を0に設定

--- backend/database/migrations/2021_09_18_194324_add_admin_to_user_table.php
+++ backend/database/migrations/2021_09_18_194324_add_admin_to_user_table.php
@@ -14,7 +14,7 @@ class AddAdminToUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('admin')->nullable();
+            $table->integer('admin')->default(0);
         });
     }
 
