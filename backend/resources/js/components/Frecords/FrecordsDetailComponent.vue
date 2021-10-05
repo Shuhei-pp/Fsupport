723a9e5e913dc9b84c6382cf3efde98666843c68
Shuhei-pp <lotakotayota@gmail.com>
2021-10-05 16:35:48 +0900

    バックエンドとのAPI繋ぎ合わせ実装

--- backend/resources/js/components/Frecords/FrecordsDetailComponent.vue
+++ backend/resources/js/components/Frecords/FrecordsDetailComponent.vue
@@ -1,3 +1,40 @@
 <template>
-  
+  <div class="container">
+    <div class="media-left">
+      <img class="media-object" width="150px" :src="'/storage/result_images/'+frecord[0].image_name">
+    </div>
+    <div class="media-body">
+      <div class="container">
+        <p>エリア: {{ frecord[0].area_name }}</p>
+        <p>日時: {{ frecord[0].datetime }}</p>
+        <p>ユーザー名: 匿名さん</p>
+        <hr>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  export default {
+    props: {
+      frecordId: String
+    },
+    data: function(){
+      return {
+        frecord: [],
+      }
+    },
+    methods: {
+      getFrecord(){
+        axios.get('/api/frecord/'+this.frecordId)
+          .then((res) => {
+            this.frecord = res.data;
+            console.log(this.frecord);
+          });
+      }
+    },
+    mounted() {
+      this.getFrecord();
+    }
+  }
+</script>
--- backend/resources/js/components/HomeComponent.vue
+++ backend/resources/js/components/HomeComponent.vue
@@ -1,59 +1,59 @@
 <template>
 
-<div>
-  <div class="container">
-    <div class="card">
-      <div class="card-header">Fishing Indexとは</div>
-      <div class="card-body">
-        <p>Fishing Indexとは釣果指数のことで風速、潮の動き、日の出、日没等のデータをもとに”釣れる”を指数化したものです。</p>
-        <p>管理人は北陸地方出身なのでこれからそこら辺を中心にエリアを広げていく予定です。</p>
-        <p>FIは季節、日の出没、風速、潮の満干、空全体の雲の割合の5つから100点満点で計算を行っています。</p>
+  <div>
+    <div class="container">
+      <div class="card">
+        <div class="card-header">Fishing Indexとは</div>
+        <div class="card-body">
+          <p>Fishing Indexとは釣果指数のことで風速、潮の動き、日の出、日没等のデータをもとに”釣れる”を指数化したものです。</p>
+          <p>管理人は北陸地方出身なのでこれからそこら辺を中心にエリアを広げていく予定です。</p>
+          <p>FIは季節、日の出没、風速、潮の満干、空全体の雲の割合の5つから100点満点で計算を行っています。</p>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="container mt-5">
-    <div class="col-6 mx-auto">
-      <div v-for="(bigarea, index) in bigareas" :key="index">
-        <h5>{{ bigarea.bigarea_name }}</h5>
+    <div class="container mt-5">
+      <div class="col-6 mx-auto">
+        <div v-for="(bigarea, index) in bigareas" :key="index">
+          <h5>{{ bigarea.bigarea_name }}</h5>
 
-        <ul class="list-unstyled" v-for="(area, index) in areas" :key="index">
-          <li v-if="area.bigarea_id == bigarea.bigarea_id">
-            <router-link v-bind:to="{name: 'area.show', params: {areaId: area.id}}">
-            {{ area.area_name }}
-            </router-link>
-          </li>
-        </ul>
+          <ul class="list-unstyled" v-for="(area, index) in areas" :key="index">
+            <li v-if="area.bigarea_id == bigarea.bigarea_id">
+              <router-link v-bind:to="{name: 'area.show', params: {areaId: area.id}}">
+              {{ area.area_name }}
+              </router-link>
+            </li>
+          </ul>
 
 
+        </div>
+          <a href="/area/new/edit"><button class="btn btn-primary">エリア追加を行う</button></a>
       </div>
-        <a href="/area/new/edit"><button class="btn btn-primary">エリア追加を行う</button></a>
     </div>
-  </div>
-  <div class="container mt-5">
-    <div class="card">
-      <div class="card-header">
-        最近の釣果投稿
-      </div>
-      <div class="card-body">
-        <div class="media" v-for="(frecord, index) in frecords" :key="index">
-          <router-link v-bind:to="{name: 'frecord.detail', params: {frecordId: frecord.frecord_id}}">
-            <div class="media-left" href="#">
-              <img class="media-object" width="150px" :src="'storage/result_images/'+frecord.image_name">
-            </div>
-            <div class="media-body">
-              <div class="container">
-                <p>エリア:{{ frecord.area_name }}</p>
-                <p>日時:{{ frecord.datetime }}</p>
-                <p>ユーザー名: 匿名さん</p>
-                <hr>
+    <div class="container mt-5">
+      <div class="card">
+        <div class="card-header">
+          最近の釣果投稿
+        </div>
+        <div class="card-body">
+          <div class="media" v-for="(frecord, index) in frecords" :key="index">
+            <router-link v-bind:to="{name: 'frecord.detail', params: {frecordId: frecord.frecord_id}}">
+              <div class="media-left" href="#">
+                <img class="media-object" width="150px" :src="'/storage/result_images/'+frecord.image_name">
               </div>
-            </div>
-          </router-link>
+              <div class="media-body">
+                <div class="container">
+                  <p>エリア:{{ frecord.area_name }}</p>
+                  <p>日時:{{ frecord.datetime }}</p>
+                  <p>ユーザー名: 匿名さん</p>
+                  <hr>
+                </div>
+              </div>
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
   
 </template>
 
