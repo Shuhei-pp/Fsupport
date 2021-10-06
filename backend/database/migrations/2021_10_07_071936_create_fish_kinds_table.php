3a505b2aaef53285fe2565068d3e64f71f3effbc
Shuhei-pp <lotakotayota@gmail.com>
2021-10-07 07:25:53 +0900

    釣果登録の際に釣れた魚を登録するtable作成

---
+++ backend/database/migrations/2021_10_07_071936_create_fish_kinds_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFishKindsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fish_kinds', function (Blueprint $table) {
+            $table->id('fish_id');
+            $table->string('fish_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fish_kinds');
+    }
+}
