3fce4a852d94e3954d7ecc968a3f56b672ebf534
Shuhei-pp <lotakotayota@gmail.com>
2021-10-14 12:39:44 +0900

    釣果検索機能API繋ぎあわせ

--- backend/resources/js/components/Frecords/FrecordSearchComponent.vue
+++ backend/resources/js/components/Frecords/FrecordSearchComponent.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="container">
 
+    <div class="p-3">
+      <h2>釣果絞り込みページ</h2>
+      <div class="p-2 bg-white border rounded">
+        <div class="row">
+          <div class="col-md-auto">
+            <p>魚種で絞り込み</p>
+            <div class="" v-for="fish in fishes" :key="fish.id">
+              <input type="checkbox" class="">{{ fish.fish_name }}
+            </div>
+          </div>
+
+          <div class="col-md-auto">
+            <p>エリアで絞り込み</p>
+            <div class="" v-for="area in areas" :key="area.id">
+              <input type="checkbox" class="">{{ area.area_name }}
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <input type="radio" name="period" checked>1週間以内
+          <input type="radio" name="period">2週間以内
+          <input type="radio" name="period">3週間以内
+          <input type="radio" name="period">全ての期間
+        </div>
+        </div>
+    </div>
+
     <div class="card">
       <div class="card-header">
         釣果検索結果
@@ -31,5 +58,24 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    data: function() {
+      return {
+        fishes: [],
+        areas: []
+      }
+    },
+    methods: {
+      getOptions(){
+        axios.get('/api/search/option')
+          .then((res) => {
+            this.fishes = res.data.fish;
+            this.areas = res.data.areas;
+          });
+      }
+    },
+    mounted() {
+      this.getOptions();
+    }
+  }
 </script>
